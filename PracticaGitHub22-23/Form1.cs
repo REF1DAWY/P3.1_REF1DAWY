13017afc39b3b271922e7f9ce9ba6b89cb9e6bfc
REF1DAWY <robespfer@alu.edu.gva.es>
2022-12-04 21:44:30 +0100

    2.0 Codigo telegrama con errores

--- PracticaGitHub22-23/Form1.cs
+++ PracticaGitHub22-23/Form1.cs
@@ -36,12 +36,13 @@ namespace PracticaGitHub22_23
             //Si el telegrama es ordinario
             if ( tipoTelegrama == 'o' ) 
                 if (numPalabras <= 10) coste = 25;  
-                else coste = 0.5*numPalabras; 
+                else coste = 0.5 * numPalabras; 
             
             else //Si el telegrama es urgente
                  if ( tipoTelegrama == 'u' )  
                 if (numPalabras <= 10) coste = 5;  
                 else coste = 5 + 0.75 * (numPalabras - 10);  
+
             else coste = 0;  txtPrecio.Text = coste.ToString() + " euros";
         }
     }
